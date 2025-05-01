331a22b994aed1f32313c50c0e15751611200bbd
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-05-01 06:47:46 +0530

    add logout, and update

--- server/src/controller/User.ts
+++ server/src/controller/User.ts
@@ -1,5 +1,5 @@
 import { generateToken } from "../lib/generateToken";
-import ChatModel from "../model/ChatSchema";
+
 import UserModel from "../model/UserSchema";
 import bcrypt from "bcryptjs"
 
@@ -43,3 +43,24 @@ export const Login = async(req:any,res:any) => {
     }
 }
 
+export const Logout = (req:any,res:any) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logout Successfully" });
+    } catch (e:any) {
+        console.error(e.message);
+        res.status(500).json({message:"Internal server error while logout"})
+    }
+}
+
+export const GetMe = async(req:any,res:any) => {
+  
+    try {
+        const userId = req.user.id;
+        const user = await UserModel.findById({ userId }).select("-password");
+        res.status(200).json({ message: "fetch user details" }, { data: user });
+    } catch (e:any) {
+        console.error("Error:",e.message);
+        res.status(500).json({message:"Internal server error while fetching user details"})
+    }
+}
--- server/src/router/UserRouter.ts
+++ server/src/router/UserRouter.ts
@@ -1,11 +1,12 @@
 import express from "express"
-import { Login, Signup } from "../controller/User";
+import { GetMe, Login, Logout, Signup } from "../controller/User";
 import { Authenticate } from "../middleware/Auth";
 
 const router = express.Router();
 
 router.post("/login", Login);
 router.post("/signup", Signup);
-
+router.post("/logout", Logout);
+router.get("/me", GetMe);
 
 export default router;
